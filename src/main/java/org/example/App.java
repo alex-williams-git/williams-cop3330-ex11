5a81100e2c62f51fa17e4d2da74efd3da0c71f4c
Alexander Williams <alexander.williams@knights.ucf.edu>
2021-09-08 20:46:35 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,20 @@
+package org.example;
+import java.util.Scanner;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("How many euros are you exchanging? ");
+        int euros = scan.nextInt();;
+
+        System.out.print("What is the exchange rate? ");
+        double rate = scan.nextDouble();
+
+        double usd = Math.round((euros * rate) * 100.0) / 100.0;
+
+        System.out.println(euros + " euros at an exchange rate of " + rate + " is " + usd + " U.S. dollars.");
+    }
+}
